1a596de807cbd6f6a14620f9ee9b0106a5c36f7c
theCYBERbro <theCYBERbro@users.noreply.github.com>
2021-02-04 23:40:22 +0200

    added: js function
    to make better changes in the future, the function is not fully implemented yet

--- StudentApp/src/assets/JS/core.js
+++ StudentApp/src/assets/JS/core.js
@@ -1,9 +1,9 @@
-window.onload = function () {
+window.onload = function() {
 
     /**
      * Put your JavaScript file
      * It will be executed, when website is loaded.
-    */
+     */
 
 
 };
@@ -13,12 +13,17 @@ $(window).load(function() {
     $(".se-pre-con").fadeOut("slow");
 });
 
-function updateCurrentYear(){//by theCYBERbro
+function aboutHeaderText() {
+    aboutTxt = document.getElementById('ab_txt');
+    aboutTxt.innerHTML = "This app is designed to help young students with learning. The app can give a lot of good sources to learn and to make your homework dumbass."
+}
+
+function updateCurrentYear() { //by theCYBERbro
     // var year;
     // var date = new Date();   
     // year = date.getFullYear();
 
     document.getElementById("year").innerHTML = new Date().getFullYear();
     //$("#year").html(year);
-  
-   }
+
+}
