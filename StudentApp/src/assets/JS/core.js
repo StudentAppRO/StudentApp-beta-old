365cdc4b8891e9dc9c376e9cccdfe5ffb7589af5
theCYBERbro <theCYBERbro@users.noreply.github.com>
2021-02-07 17:21:09 +0200

    🦧updated buttons CSS

--- StudentApp/src/assets/JS/core.js
+++ StudentApp/src/assets/JS/core.js
@@ -6,6 +6,8 @@ window.onload = function() {
      * Put your JavaScript file
      * It will be executed, when website is loaded.
      */
+    aboutHeaderText();
+    updateCurrentYear();
     getCurrentDate();
     getUserLocation();
 };
