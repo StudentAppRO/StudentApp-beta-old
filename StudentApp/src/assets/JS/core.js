1ddcd1c256636171f80c92e4a1702be1a8a5741a
thecyberbro <thecyberbro@gmail.com>
2021-07-11 11:16:01 +0300

    🐜animation + bugfix

--- StudentApp/src/assets/JS/core.js
+++ StudentApp/src/assets/JS/core.js
@@ -92,13 +92,8 @@ function urlParams() {
 }
 
 function updateCurrentYear() {
-  //by theCYBERbro
-  // var year;
-  // var date = new Date();
-  // year = date.getFullYear();
 
   document.getElementById("year").innerHTML = new Date().getFullYear();
-  //$("#year").html(year);
 }
 
 function getCurrentDate() {
